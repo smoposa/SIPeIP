7d8a468b7e7b61b400f5c65b1c680b2b438775c1
Santiago Moposa <sdmcompu@hotmail.com>
2026-08-24 19:10:42 -0500

    fix: registrar bindings de repositories en AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,25 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+// PND
 use App\Repositories\Contracts\PndRepositoryInterface;
 use App\Repositories\Eloquent\PndRepository;
 
+// ODS
 use App\Repositories\Contracts\OdsRepositoryInterface;
 use App\Repositories\Eloquent\OdsRepository;
+
+// Roles
+use App\Repositories\Contracts\RoleRepositoryInterface;
+use App\Repositories\Eloquent\RoleRepository;
+
+// Usuarios
+use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Repositories\Eloquent\UserRepository;
+
+// Entidades
+use App\Repositories\Contracts\EntidadRepositoryInterface;
+use App\Repositories\Eloquent\EntidadRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +41,24 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             OdsRepositoryInterface::class,
             OdsRepository::class
+        );
+
+        // Roles
+        $this->app->bind(
+            RoleRepositoryInterface::class,
+            RoleRepository::class
+        );
+
+        // Usuarios
+        $this->app->bind(
+            UserRepositoryInterface::class,
+            UserRepository::class
+        );
+
+        // Entidades
+        $this->app->bind(
+            EntidadRepositoryInterface::class,
+            EntidadRepository::class
         );
     }
 
